f5ad9e253c823cd05d291cb054f510e4981c0592
Omer Gamliel <omergamliel3@gmail.com>
2020-09-07 19:02:11 +0300

    commit

--- lib/core/widgets/ui_elements/status_modal_bottom_sheet.dart
+++ lib/core/widgets/ui_elements/status_modal_bottom_sheet.dart
@@ -10,125 +10,139 @@ import 'package:WhatsAppClone/services/local_storage/prefs_service.dart';
 
 import 'package:WhatsAppClone/core/widgets/ui_elements/spinkit_loading_indicator.dart';
 
-// text field controller
-final TextEditingController _textEditingController = TextEditingController();
-// responsive widget
-Widget _responsiveWidget;
-// responsive widget flag
-bool flag = true;
-
 // parent container border radius
 final borderRadius = RoundedRectangleBorder(
     borderRadius: BorderRadius.only(
         topLeft: const Radius.circular(8.0),
         topRight: const Radius.circular(8.0)));
 
-// inner child container border radius
-final borderRadiusGeometry = BorderRadius.only(
-    topLeft: const Radius.circular(8.0), topRight: const Radius.circular(8.0));
-
-// build status text field
-Widget _buildTextField(BuildContext context) {
-  final bool isLight = Theme.of(context).brightness == Brightness.light;
-  return Container(
-    height: 50,
-    alignment: Alignment.center,
-    padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-    decoration: BoxDecoration(
-        color: isLight ? Colors.grey[300] : Colors.grey[600],
-        borderRadius: BorderRadius.circular(10)),
-    child: TextField(
-      controller: _textEditingController,
-      keyboardType: TextInputType.text,
-      autofocus: true,
-      decoration: InputDecoration.collapsed(hintText: 'Enter your status'),
-    ),
-  );
-}
-
-// build upload status button
-Widget _buildUploadButton(
-    BuildContext context, void Function(void Function()) setState) {
-  return FlatButton(
-    child: Text(
-      'UPLOAD STATUS',
-    ),
-    onPressed: () {
-      _updateStatus(context, setState);
-    },
-  );
-}
-
-// update status in FirestoreService, PrefsService, MainModel provider
-void _updateStatus(
-    BuildContext context, void Function(void Function()) setState) async {
-  if (_textEditingController.value.text == null ||
-      _textEditingController.value.text.isEmpty) return;
-  Status status = Status(
-      userName: PrefsService.userName,
-      content: _textEditingController.value.text,
-      timestamp: DateTime.now());
-
-  setState(() {
-    _responsiveWidget = Center(
-      child: SpinkitLoadingIndicator(),
-    );
-  });
-
-  // upload status to firestore db
-  bool upload = await FirestoreService.uploadStatus(status);
-  if (upload) {
-    // reset flag
-    flag = true;
-    // save user status localy
-    PrefsService.saveUserStatus(status: status.content);
-    // update user status in main model
-    context.read<MainModel>().updateUserStatus();
-    if (Navigator.canPop(context)) {
-      Navigator.pop(context);
-    }
-  } else {
-    // reset flag and setState to update responsive widget back to button
-    setState(() {
-      flag = true;
-    });
-  }
-}
-
 void showStatusModalBottomSheet(BuildContext context) {
-  final targetSize = MediaQuery.of(context).size.height * 0.2;
   showModalBottomSheet(
       context: context,
       isScrollControlled: true,
       shape: borderRadius,
       builder: (context) {
-        return StatefulBuilder(builder:
-            (BuildContext context, void Function(void Function()) setState) {
-          // execute this code one time only
-          if (flag) _responsiveWidget = _buildUploadButton(context, setState);
-          flag = false;
-          return SingleChildScrollView(
-            child: Container(
-              padding: EdgeInsets.only(
-                  bottom: MediaQuery.of(context).viewInsets.bottom),
-              decoration: BoxDecoration(borderRadius: borderRadiusGeometry),
-              child: Container(
-                height: targetSize,
-                padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    _buildTextField(context),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    _responsiveWidget
-                  ],
-                ),
-              ),
-            ),
-          );
-        });
+        return ModalBottomSheetScreen();
       });
+}
+
+class ModalBottomSheetScreen extends StatefulWidget {
+  @override
+  _ModalBottomSheetScreenState createState() => _ModalBottomSheetScreenState();
+}
+
+class _ModalBottomSheetScreenState extends State<ModalBottomSheetScreen> {
+  // text field controller
+  final TextEditingController _textEditingController = TextEditingController();
+  // responsive widget
+  Widget _responsiveWidget;
+
+  @override
+  void initState() {
+    _responsiveWidget = _buildUploadButton();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
+// inner child container border radius
+  final borderRadiusGeometry = BorderRadius.only(
+      topLeft: const Radius.circular(8.0),
+      topRight: const Radius.circular(8.0));
+
+  // build status text field
+  Widget _buildTextField(BuildContext context) {
+    final bool isLight = Theme.of(context).brightness == Brightness.light;
+    return Container(
+      height: 50,
+      alignment: Alignment.center,
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      decoration: BoxDecoration(
+          color: isLight ? Colors.grey[300] : Colors.grey[600],
+          borderRadius: BorderRadius.circular(10)),
+      child: TextField(
+        controller: _textEditingController,
+        keyboardType: TextInputType.text,
+        autofocus: true,
+        decoration: InputDecoration.collapsed(hintText: 'Enter your status'),
+      ),
+    );
+  }
+
+// build upload status button
+  Widget _buildUploadButton() {
+    return FlatButton(
+      child: Text(
+        'UPLOAD STATUS',
+      ),
+      onPressed: () {
+        _updateStatus();
+      },
+    );
+  }
+
+// update status in FirestoreService, PrefsService, MainModel provider
+  void _updateStatus() async {
+    if (_textEditingController.value.text == null ||
+        _textEditingController.value.text.isEmpty) return;
+    Status status = Status(
+        userName: PrefsService.userName,
+        content: _textEditingController.value.text,
+        timestamp: DateTime.now());
+
+    setState(() {
+      _responsiveWidget = Center(
+        child: SpinkitLoadingIndicator(),
+      );
+    });
+
+    // upload status to firestore db
+    bool upload = await FirestoreService.uploadStatus(status);
+    if (upload) {
+      // save user status localy
+      PrefsService.saveUserStatus(status: status.content);
+      // update user status in main model
+      context.read<MainModel>().updateUserStatus();
+      if (Navigator.canPop(context)) {
+        Navigator.pop(context);
+      }
+    } else {
+      // reset flag and setState to update responsive widget back to button
+      setState(() {
+        _responsiveWidget = _buildUploadButton();
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // execute this code one time only
+    final targetSize = MediaQuery.of(context).size.height * 0.2;
+    return SingleChildScrollView(
+      child: Container(
+        padding:
+            EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+        decoration: BoxDecoration(borderRadius: borderRadiusGeometry),
+        child: Container(
+          height: targetSize,
+          padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              _buildTextField(context),
+              SizedBox(
+                height: 10,
+              ),
+              _responsiveWidget
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
--- lib/services/firebase/firestore_service.dart
+++ lib/services/firebase/firestore_service.dart
@@ -5,7 +5,8 @@ import 'package:WhatsAppClone/core/models/status.dart';
 import 'package:WhatsAppClone/helpers/connectivity_helper.dart';
 
 abstract class FirestoreService {
-  static const _kCollectionName = 'users_status';
+  static const _kUsersStatusCollection = 'users_status';
+  static const _kUserNamesCollection = 'user_names';
 
   /// add new user status to firestore db collection
   static Future<bool> uploadStatus(Status status) async {
@@ -18,7 +19,7 @@ abstract class FirestoreService {
     try {
       // add new status object to users_status db collection
       var docRef = await FirebaseFirestore.instance
-          .collection(_kCollectionName)
+          .collection(_kUsersStatusCollection)
           .add(status.toJsonMap());
 
       print('created new firestore recored with id: ${docRef.id}');
@@ -29,7 +30,7 @@ abstract class FirestoreService {
     }
   }
 
-  /// delete status from firestore db collection
+  /// delete status from firestore db [users_status] collection
   static Future<bool> deleteStatus(Status status) async {
     // checks for internet connectivity
     bool connectivity = await ConnectivityHelper.internetConnectivity();
@@ -39,11 +40,72 @@ abstract class FirestoreService {
     }
     try {
       await FirebaseFirestore.instance
-          .collection(_kCollectionName)
+          .collection(_kUsersStatusCollection)
           .doc(status.id)
           .delete();
       return true;
     } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+
+  /// add new username to firestore db [user_names] collection
+  static Future<bool> addUserName(String username) async {
+    // checks for internet connectivity
+    bool connectivity = await ConnectivityHelper.internetConnectivity();
+    // return false if there is no connectivity
+    if (!connectivity) {
+      return false;
+    }
+    try {
+      // add new username object to user_names db collection
+      var docRef = await FirebaseFirestore.instance
+          .collection(_kUserNamesCollection)
+          .add({'username': username});
+
+      print('created new firestore recored with id: ${docRef.id}');
+      return true;
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+
+  /// compare [username] argument with [user_names] collection
+  /// returns [true] if do not exists in collection, [false] if exists
+  static Future<bool> validateUserName(String username) async {
+    // checks for internet connectivity
+    bool connectivity = await ConnectivityHelper.internetConnectivity();
+    // return false if there is no connectivity
+    if (!connectivity) {
+      return false;
+    }
+    try {
+      // get query db collection
+      QuerySnapshot query = await FirebaseFirestore.instance
+          .collection(_kUserNamesCollection)
+          .get();
+      // get query docs
+      List<QueryDocumentSnapshot> usernames = query.docs;
+      // username flag
+      bool flag = false;
+      // iterate usernames query docs list
+      usernames.forEach((element) {
+        if (element.data()['username'] == username) {
+          flag = true;
+          return;
+        }
+      });
+      // username is taken, not valid.
+      if (flag) {
+        return false;
+      }
+      // username is not taken, valid.
+      return true;
+    }
+    // return false if catch error
+    catch (e) {
       print(e);
       return false;
     }
