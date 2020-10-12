108b8b521946a85620659ee897eac46fd69bda74
Omer Gamliel <omergamliel3@gmail.com>
2020-10-12 16:04:29 +0300

    update assets contants

--- lib/core/shared/constants.dart
+++ lib/core/shared/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-const String backGroundAssetName = 'asset/images/Whatsapp_Wallpaper.jpg';
-const String iconAssetName = 'asset/images/whatsapp_icon.png';
+const String backGroundAssetName = 'assets/images/Whatsapp_Wallpaper.jpg';
+const String iconAssetName = 'assets/images/whatsapp_icon.png';
 const Color kPrimaryColor = Color(0xff075E54);
 const Color kAccentColor = Color(0xff25D366);
 
