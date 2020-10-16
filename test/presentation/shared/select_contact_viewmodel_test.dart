62b37ed89a6df33b4e7001ea80ac107b47635a46
Omer Gamliel <omergamliel3@gmail.com>
2020-10-16 22:54:29 +0300

    add viewmodel methods tests

--- test/presentation/shared/select_contact_viewmodel_test.dart
+++ test/presentation/shared/select_contact_viewmodel_test.dart
@@ -1,9 +1,12 @@
 import 'package:WhatsAppClone/core/models/contact_entity.dart';
+import 'package:WhatsAppClone/core/routes/routing_constants.dart';
+import 'package:WhatsAppClone/presentation/index.dart';
 import 'package:WhatsAppClone/presentation/shared/select_contact_viewmodel.dart';
 import 'package:WhatsAppClone/services/index.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:stacked_services/stacked_services.dart';
+import '../../data/repositories/test_data.dart';
 import '../../test_helper.dart';
 
 void main() {
@@ -12,6 +15,8 @@ void main() {
   ContactsRepository contactsRepo;
   AnalyticsService analytics;
   DialogService dialogService;
+  NavigationService navigator;
+
   // dummy contact entity data
   ContactEntity entity;
   setUp(() {
@@ -19,7 +24,7 @@ void main() {
     contactsRepo = getAndRegisterContactsRepositoryMock();
     analytics = getAndRegisterAnalyticsServiceMock();
     dialogService = getAndRegisterDialogServiceMock();
-    getAndRegisterNavigationServiceMock();
+    navigator = getAndRegisterNavigationServiceMock();
     entity = ContactEntity(
         displayName: 'omer',
         phoneNumber: '00000',
@@ -31,14 +36,14 @@ void main() {
     /// [activateContact method test (activate success)]
     test('activate contact with success', () async {
       // mock activate contact to return true (success)
-      when(contactsRepo.activateContact(entity))
+      when(contactsRepo.activateContact(contactEntity))
           .thenAnswer((realInvocation) => Future.value(true));
 
       // construct model
       final model = SelectContactViewModel();
-      await model.activateContact(entity);
+      await model.activateContact(contactEntity);
       verifyInOrder([
-        contactsRepo.activateContact(entity),
+        contactsRepo.activateContact(contactEntity),
         analytics.logCreateNewContactEvent(),
         router.pop()
       ]);
@@ -59,6 +64,69 @@ void main() {
           description: 'failed to create new chat, please try again.'));
       verifyNever(router.pop());
       verifyNever(analytics.logCreateNewContactEvent());
+    });
+
+    /// [getViewContacts method test]
+    test(
+        '''when call getViewContacts should return List of type ContactEntity according to the Contacts mode argument''',
+        () {
+      // construct tested viewmodel
+      final model = SelectContactViewModel();
+      // mock repo
+      when(contactsRepo.activeContacts)
+          .thenAnswer((realInvocation) => [contactEntity]);
+      when(contactsRepo.unActiveContacts)
+          .thenAnswer((realInvocation) => localDatabaseContactsEntites);
+      // evoke tested method
+      List<ContactEntity> contacts = model.getViewContacts(ContactMode.chat);
+      expect(contacts, equals(localDatabaseContactsEntites));
+
+      contacts = model.getViewContacts(ContactMode.calls);
+      expect(contacts, equals([contactEntity] + localDatabaseContactsEntites));
+
+      contacts = model.getViewContacts(ContactMode.setImage);
+      expect(contacts, equals([contactEntity] + localDatabaseContactsEntites));
+    });
+
+    /// [sendImage method test (contact is active and success inserted)]
+    test('''
+         when call sendImage(), should evoke activateContact() if contactEntity argument is not active, then construct new Mesasge instance and evoke insertMessage(). If inserted, evoke setActiveContacts, and navigate to privateChatRoute. Else, show failed Dialog''',
+        () async {
+      // construct tested model
+      final model = SelectContactViewModel();
+      // mock unActiveContacts from contacts repo
+      when(contactsRepo.unActiveContacts)
+          .thenAnswer((realInvocation) => localDatabaseContactsEntites);
+      when(contactsRepo.insertMessage(any)).thenAnswer((realInvocation) =>
+          Future.value(true)); // evoke and await tested method
+      await model.sendImage('path', contactEntity);
+      verifyNever(contactsRepo.activateContact(contactEntity));
+      verifyInOrder([
+        contactsRepo.setActiveContacts(),
+        navigator.clearTillFirstAndShow(privateChatRoute,
+            arguments: contactEntity.toJsonMap())
+      ]);
+    });
+
+    /// [sendImage method test (contact is un active and inserted failure)]
+    test('''
+         when call sendImage(), should evoke activateContact() if contactEntity argument is not active, then construct new Mesasge instance and evoke insertMessage(). If inserted, evoke setActiveContacts, and navigate to privateChatRoute. Else, show failed Dialog''',
+        () async {
+      // construct tested model
+      final model = SelectContactViewModel();
+      // mock unActiveContacts from contacts repo
+      when(contactsRepo.unActiveContacts)
+          .thenAnswer((realInvocation) => [contactEntity]);
+      when(contactsRepo.activateContact(contactEntity))
+          .thenAnswer((realInvocation) => Future.value(true));
+      when(contactsRepo.insertMessage(any)).thenAnswer((realInvocation) =>
+          Future.value(false)); // evoke and await tested method
+      await model.sendImage('path', contactEntity);
+      verifyInOrder([
+        contactsRepo.activateContact(contactEntity),
+        dialogService.showDialog(
+            title: 'Failed to send image', description: 'Please try again')
+      ]);
     });
   });
 }
